300cc92f32e69dcc537e758571214cec9311e63f
abisha90 <165835165+abisha90@users.noreply.github.com>
2024-05-13 16:43:29 -0400

    navigation js complete

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+// my selector for the burger button
+var burgerBtn = document.querySelector("#burger-btn");
+var mainNav = document.querySelector("#main-nav");
+
+var isNavVisible = false; 
+
+burgerBtn.addEventListener("click", () => {
+    // console.log("button working!!!");
+
+    if (isNavVisible === false) {
+        mainNav.classList.remove("hide-menu");
+        isNavVisible = true;
+    }else{
+        mainNav.classList.add("hide-menu");
+        isNavVisible = false;
+    }
+})
+
+mainNav.addEventListener("click", () => {
+    mainNav.classList.add("hide-menu");
+    isNavVisible = false;
+})
